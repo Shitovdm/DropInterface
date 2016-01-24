4b138f26abcf487a4bca2e652f498ec693c6aa08
Dmitry Shitov <shitov.dm@gmail.com>
2016-01-24 23:48:10 +0300

    Словарь языков.

---
+++ js/lang.js
@@ -0,0 +1,30 @@
+
+dictionary = new Array(
+	{ // en
+		// Header
+		Main : "Main",
+		Instruction : "Instruction",
+		Payment : "Payment",
+		Log_out : "Log out",
+		// Main page
+		add_account : "Add account"
+	},	
+	{ // ru
+		// Header
+		Main : "Главная",
+		Instruction : "Инструкция",
+		Payment : "Оплата",
+		Log_out : "Выход",
+		// Main page
+		add_account : "Добавить аккаунт",
+		add_accounts_block : "Добавить блок аккаунтов(если доступно)",
+		minimize : "Свернуть",
+		update_accounts_status : "Обновить все статусы",
+		auto_update_accounts_status: "Автоматически обновлять статусы аккаунтов",
+		edit_accounts : "Редактирование информации",
+		show_in_blocks : "Отобразить в виде блоков",
+		show_in_list : "Отобразить в виде списка",
+		show_in_console : "Отобразитов консоле"
+	}
+	
+);
