c8ee6c9757ae2cf3d4ea7e916cc8b34b02bb3a75
Dmitry Shitov <shitov.dm@gmail.com>
2016-02-04 18:36:34 +0300

    Словарь обновленный(в процессе составления)

--- js/lang.js
+++ js/lang.js
@@ -7,7 +7,11 @@ dictionary = new Array(
 		Payment : "Payment",
 		Log_out : "Log out",
 		// Main page
-		add_account : "Add account"
+		add_account : "Add account",
+		
+		
+		
+		console : "Console"
 	},	
 	{ // ru
 		// Header
@@ -18,13 +22,66 @@ dictionary = new Array(
 		// Main page
 		add_account : "Добавить аккаунт",
 		add_accounts_block : "Добавить блок аккаунтов(если доступно)",
+		// Tools
 		minimize : "Свернуть",
 		update_accounts_status : "Обновить все статусы",
 		auto_update_accounts_status: "Автоматически обновлять статусы аккаунтов",
 		edit_accounts : "Редактирование информации",
+		delete_account : "Удалить аккаунт",
 		show_in_blocks : "Отобразить в виде блоков",
 		show_in_list : "Отобразить в виде списка",
-		show_in_console : "Отобразитов консоле"
+		show_console : "Показать консоль",
+		show_all_items : "Показать все коробки на аккаунтах",
+		show_modal_window : "Показывать коробки на аккаунтах",
+		update_inventories : "Обновить инвентари аккаунтов",
+		// Tips & Tricks
+		tips_and_tricks : "Отключить подсказки",
+		download_db : "Скачать базу данных",
+		// Errors and messages
+		error_adding_account : "Добавление аккаунтов невозможно. Достигнут предел аккаунтов.",
+		error_adding_block : "Создать блок невозможно, так как предыдущий еще не заполнен",
+		error_addint_block_payment : "Достигнут предел аккаунтов. Произведите оплату за дополнительное пространство.",
+		error_deleting_account : "Удаление аккаунта невозможно, повторите позже или обратитель в службу поддержки.",
+		error_loading_userInventory : "Ошибка при загрузке инвентаря пользователя, повторите попытку позже.",
+		message_turnON_editor : "Включен режим редактирования полей",
+		message_turnON_deleting : "Включен режим удаления аккаунтов",
+		message_turnOFF_tips : "Подсказки отключены",
+		message_turnON_tips : "Подсказки включены",
+		message_turnON_modalUserInventory : "Включен просмотр инвентарей пользователей. Для просмотра инвентаря навелите на порядковый номер аккаунта.",
+		message_turnON_console : "Консоль включена",
+		message_turnOFF_console : "Консоль отключена",
+		// Консоль
+		console : "Консоль",
+		console_help : "Для вывода справки введите команду \"help\"",
+		
+		console_color : "color [attr]</span>" + 
+						"<div class='clear'></div>" +
+						"<span> </span>" +
+						"<div class='clear'></div>" +
+						"<span>Атрибуты цветов задаются в виде ДВУХ шестнадцатеричных цифр — первая</span>" +
+						"<div class='clear'></div>" +
+						"<span>задает цвет фона, а вторая определяет цвет переднего плана. Каждая цифра</span>" +
+						"<div class='clear'></div>" +
+						"<span>может иметь следующие значения:</span>" +
+						"<div class='clear'></div>" +
+						"<span> </span>" +
+						"<div class='clear'></div>" +
+						"<span>0 = Черный 	5 = Лиловый</span>" +
+						"<div class='clear'></div>" +
+						"<span>1 = Синий	6 = Желтый</span>" +
+						"<div class='clear'></div>" +
+						"<span>2 = Зеленый	7 = Белый</span>" +
+						"<div class='clear'></div>" +
+						"<span>3 = Голубой	8 = Серый</span>" +
+						"<div class='clear'></div>" +
+						"<span>4 = Красный</span>" +
+						"<div class='clear'></div>" +
+						"<span> </span>" +
+						"<div class='clear'></div>" +
+						"<span>Пример: \"color 02\" задает зелёный передний план на черном фоне",
+		console_help : "Help me please!"
+
 	}
+	
 	
 );
